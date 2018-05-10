5c2f29eee0f88fb1c0539d06854ab0e13ef35244
theprogramer9 <38675543+theprogramer9@users.noreply.github.com>
2018-05-10 00:06:44 -0500

    commit

--- app/src/main/java/com/robeasd/takepicture/NavigationDrawer.java
+++ app/src/main/java/com/robeasd/takepicture/NavigationDrawer.java
@@ -55,7 +55,7 @@ public class NavigationDrawer extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present hola mundo.
         getMenuInflater().inflate(R.menu.navigation_drawer, menu);
         return true;
     }
